06682e5f7e1f8217fedd47f0f1cc0e7762533a7a
wakai-megumi <tarun483sharma@gmail.com>
2023-05-19 14:31:25 +0530

    add

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
 
-    origin: ["http://localhost:5174/"],
+    origin: ["*"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 
