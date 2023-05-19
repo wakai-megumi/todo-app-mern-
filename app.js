81f92603e73b3ff0a81a86d48fdf1b9d1dddf08b
wakai-megumi <tarun483sharma@gmail.com>
2023-05-19 14:43:49 +0530

    final update to cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
 
-    origin: "*",
+    origin: ['http://localhost:5174'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 
