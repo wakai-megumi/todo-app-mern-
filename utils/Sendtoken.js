6791b0d8262097584d1a8a27b1f70e5df09e9d36
wakai-megumi <110999461+wakai-megumi@users.noreply.github.com>
2023-05-20 16:26:26 +0530

    Update Sendtoken.js

--- utils/Sendtoken.js
+++ utils/Sendtoken.js
@@ -19,7 +19,7 @@ const SendToken = (user, statusCode = 200, res, message) => {
         return res.status(200).cookie("accesstoken", token, {
             httpOnly: true,
             maxAge: 15 * 60 * 1000,
-            sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+            sameSite: "lax",
             secure: false
         }).json({
             success: true,
